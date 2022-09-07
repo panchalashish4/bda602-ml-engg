9f6e3e7cb92d3ed545c026d8b1db56e056923466
panchalashish4 <panchalashish4@gmail.com>
2022-09-06 19:28:58 -0700

    Initial Commit for HW1
    HW1: Added python file with initial code for reading data and calculating stats.

---
+++ code/hw1.py
@@ -0,0 +1,66 @@
+import sys
+
+import numpy as np
+import pandas as pd
+import plotly.express as px
+
+
+def read_data():
+    url = "https://teaching.mrsharky.com/data/iris.data"
+    cols = ["sepal_length", "sepal_width", "petal_length", "petal_width", "class"]
+    df = pd.read_csv(url, names=cols, header=None)
+    return df
+
+
+def stats(df):
+    for (columnName, columnData) in df.iteritems():
+        mean = round(np.mean(columnData.values), 2)
+        min = round(np.min(columnData.values), 2)
+        max = round(np.max(columnData.values), 2)
+        quart_25 = round(np.quantile(columnData.values, 0.25), 2)
+        quart_50 = round(np.quantile(columnData.values, 0.5), 2)
+        quart_75 = round(np.quantile(columnData.values, 0.75), 2)
+        print(
+            "Column Name :",
+            columnName,
+            "\n Mean:",
+            mean,
+            " Max:",
+            max,
+            " Min:",
+            min,
+            "\n Quartiles:: 25%:",
+            quart_25,
+            " 50%:",
+            quart_50,
+            " 75%:",
+            quart_75,
+        )
+
+
+def visuals(df):
+
+    fig = px.scatter(
+        df,
+        x="sepal_width",
+        y="sepal_length",
+        color="class",
+        size="petal_length",
+        hover_data=["petal_width"],
+    )
+
+    fig1 = px.scatter(df, x="sepal_width", y="sepal_length", color="petal_length")
+    fig.show()
+    fig1.show()
+
+
+def main():
+    df = read_data()
+    X_orig = df.iloc[:, :3]
+    # df.iloc[:, -1:]
+    stats(X_orig)
+    visuals(df)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
