dc49461bdcf133ba461b57a9ccb07bef5662832c
panchalashish4 <panchalashish4@gmail.com>
2022-10-11 00:11:32 -0700

    Updated functions and added new function to save plots

--- code/hw4/hw4.py
+++ code/hw4/hw4.py
@@ -1,12 +1,14 @@
 # Import packages
 import sys
 from collections import defaultdict
+from pathlib import Path
 
 import numpy
 from pandas import Series
 from plotly import express as px
 from plotly import figure_factory as ff
 from plotly import graph_objects as go
+from plotly.graph_objs import Figure
 from pydataset import data
 from sklearn import datasets
 
@@ -120,7 +122,7 @@ def get_data():
     return data_list
 
 
-def check_response(response: Series):
+def check_response(response: Series) -> bool:
     print_subheading("Response Type")
     if len(response.unique()) > 2:
         print(f"{response.name} is continuous")
@@ -130,7 +132,7 @@ def check_response(response: Series):
         return 0
 
 
-def check_predictors(predictor: Series):
+def check_predictors(predictor: Series) -> bool:
     print_subheading("Predictor Type")
 
     if (
@@ -144,21 +146,26 @@ def check_predictors(predictor: Series):
         return 1
 
 
-def cat_response_cat_predictor(response: Series, predictor: Series):
+def cat_response_cat_predictor(
+    dataset_name: str, response: Series, predictor: Series
+) -> None:
     fig = px.density_heatmap(
         x=predictor, y=response, color_continuous_scale="Viridis", text_auto=True
     )
+    title = f"Categorical Predictor ({predictor.name}) by Categorical Response ({response.name})"
     fig.update_layout(
-        title=f"Categorical Predictor ({predictor.name}) by Categorical Response ({response.name})",
+        title=title,
         xaxis_title=f"Predictor ({predictor.name})",
         yaxis_title=f"Response ({response.name})",
     )
     # fig.show()
-
+    save_plot(dataset_name, fig, title)
     return
 
 
-def cat_resp_cont_predictor(response: Series, predictor: Series):
+def cat_resp_cont_predictor(
+    dataset_name: str, response: Series, predictor: Series
+) -> None:
     out = defaultdict(list)
     for key, value in zip(response.values, predictor.values):
         out[f"Response = {key}"].append(value)
@@ -166,16 +173,18 @@ def cat_resp_cont_predictor(response: Series, predictor: Series):
     response_values = list(out.keys())
 
     fig1 = ff.create_distplot(predictor_values, response_values, bin_size=0.2)
+    title1 = f"Continuous Predictor ({predictor.name}) by Categorical Response ({response.name})"
     fig1.update_layout(
-        title=f"Continuous Predictor ({predictor.name}) by Categorical Response ({response.name})",
+        title=title1,
         xaxis_title=f"Predictor ({predictor.name})",
         yaxis_title="Distribution",
     )
     # fig1.show()
+    save_plot(dataset_name, fig1, title1)
 
-    fig_2 = go.Figure()
+    fig2 = go.Figure()
     for curr_hist, curr_group in zip(predictor_values, response_values):
-        fig_2.add_trace(
+        fig2.add_trace(
             go.Violin(
                 x=numpy.repeat(curr_group, len(curr_hist)),
                 y=curr_hist,
@@ -184,15 +193,20 @@ def cat_resp_cont_predictor(response: Series, predictor: Series):
                 meanline_visible=True,
             )
         )
-    fig_2.update_layout(
-        title=f"Continuous Predictor ({predictor.name}) by Categorical Response ({response.name})",
+    title2 = f"Continuous Predictor ({predictor.name}) by Categorical Response ({response.name})"
+    fig2.update_layout(
+        title=title2,
         xaxis_title=f"Response ({response.name})",
         yaxis_title=f"Predictor ({predictor.name})",
     )
-    # fig_2.show()
+    # fig2.show()
+    save_plot(dataset_name, fig2, title2)
+    return
 
 
-def cont_resp_cat_predictor(response: Series, predictor: Series):
+def cont_resp_cat_predictor(
+    dataset_name: str, response: Series, predictor: Series
+) -> None:
     out = defaultdict(list)
     for key, value in zip(predictor.values, response.values):
         out[f"Predictor = {key}"].append(value)
@@ -200,16 +214,18 @@ def cont_resp_cat_predictor(response: Series, predictor: Series):
     predictor_values = list(out.keys())
 
     fig1 = ff.create_distplot(response_values, predictor_values, bin_size=0.2)
+    title1 = f"Continuous Response ({response.name}) by Categorical Predictor ({predictor.name})"
     fig1.update_layout(
-        title=f"Continuous Response ({response.name}) by Categorical Predictor ({predictor.name})",
+        title=title1,
         xaxis_title=f"Response ({response.name})",
         yaxis_title="Distribution",
     )
     # fig1.show()
+    save_plot(dataset_name, fig1, title1)
 
-    fig_2 = go.Figure()
+    fig2 = go.Figure()
     for curr_hist, curr_group in zip(response_values, predictor_values):
-        fig_2.add_trace(
+        fig2.add_trace(
             go.Violin(
                 x=numpy.repeat(curr_group, len(curr_hist)),
                 y=curr_hist,
@@ -218,26 +234,62 @@ def cont_resp_cat_predictor(response: Series, predictor: Series):
                 meanline_visible=True,
             )
         )
-    fig_2.update_layout(
-        title=f"Continuous Response ({response.name}) by Categorical Predictor ({predictor.name})",
+    title2 = f"Continuous Response ({response.name}) by Categorical Predictor ({predictor.name})"
+    fig2.update_layout(
+        title=title2,
         xaxis_title=f"Predictor ({predictor.name})",
         yaxis_title=f"Response ({response.name})",
     )
 
-    # fig_2.show()
+    # fig2.show()
+    save_plot(dataset_name, fig2, title2)
     return
 
 
-def cont_response_cont_predictor(response: Series, predictor: Series):
-
+def cont_response_cont_predictor(
+    dataset_name: str, response: Series, predictor: Series
+) -> None:
     fig = px.scatter(x=predictor, y=response, trendline="ols")
+    title = f"Continuous Response ({response.name}) by Continuous Predictor ({predictor.name})"
     fig.update_layout(
-        title=f"Continuous Response ({response.name}) by Continuous Predictor ({predictor.name})",
+        title=title,
         xaxis_title=f"Predictor ({predictor.name})",
         yaxis_title=f"Response ({response.name})",
     )
-    fig.show()
+    # fig.show()
+    save_plot(dataset_name, fig, title)
 
+    return
+
+
+def check_and_plot(dataset_name: str, response: Series, predictor: Series) -> None:
+    response_type = check_response(response)
+    predictor_type = check_predictors(predictor)
+    if response_type == 0 and predictor_type == 0:
+        cat_response_cat_predictor(dataset_name, response, predictor)
+    elif response_type == 0 and predictor_type == 1:
+        cat_resp_cont_predictor(dataset_name, response, predictor)
+    elif response_type == 1 and predictor_type == 0:
+        cont_resp_cat_predictor(dataset_name, response, predictor)
+    elif response_type == 1 and predictor_type == 1:
+        if response.dtypes not in ("int64", "float64"):
+            response_name = response.name
+            response = response.astype("category")
+            response = response.cat.codes
+            response.name = response_name
+        cont_response_cont_predictor(dataset_name, response, predictor)
+    else:
+        print("Unable to plot the datatypes!!!")
+        print(f"Response: {response.dtypes}, Predictor: {predictor.dtypes}")
+
+    return
+
+
+def save_plot(dataset_name: str, fig: Figure, name: str):
+    cd = sys.path[0]
+    path = f"{cd}/plots/{dataset_name}/"
+    Path(path).mkdir(parents=True, exist_ok=True)
+    fig.write_html(f"{path}{name}.html")
     return
 
 
@@ -246,27 +298,14 @@ def main():
     for dataset in data_list:
 
         df = dataset["dataset"]
+        dataset_name = dataset["name"]
         predictors = df[dataset["predictors"]]
         response = df[dataset["response"]]
-        print_heading(f"For {dataset['name']} dataset")
-        response_type = check_response(response)
+        print_heading(f"For {dataset_name} dataset")
 
         for col_name in predictors.columns:
             predictor = df[col_name]
-            predictor_type = check_predictors(predictor)
-            if response_type == 0 and predictor_type == 0:
-                cat_response_cat_predictor(response, predictor)
-            elif response_type == 0 and predictor_type == 1:
-                cat_resp_cont_predictor(response, predictor)
-            elif response_type == 1 and predictor_type == 0:
-                cont_resp_cat_predictor(response, predictor)
-            elif response_type == 1 and predictor_type == 1:
-                if response.dtypes not in ("int64", "float64"):
-                    response_name = response.name
-                    response = response.astype("category")
-                    response = response.cat.codes
-                    response.name = response_name
-                cont_response_cont_predictor(response, predictor)
+            check_and_plot(dataset_name, response, predictor)
 
 
 if __name__ == "__main__":
